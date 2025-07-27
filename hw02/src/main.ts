a8be573e6334f602479fa34c894d646a3f064039
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-28 01:55:48 +0300

    Update main.ts

--- hw02/src/main.ts
+++ hw02/src/main.ts
@@ -1,5 +1,31 @@
+import { Logger } from "./services/Logger";
+import { EmailNotification } from "./services/EmailNotification";
+import { SMSNotification } from "./services/SMSNotification";
+import { PushNotification } from "./services/PushNotification";
+import { NotificationService } from "./services/NotificationService";
 import { User } from "./models/User";
 
-const user = new User("example@email.com", "+380123456789", "device-token-abc");
+
+const logger = new Logger();
+
+
+const emailChannel = new EmailNotification("example@email.com", logger);
+const smsChannel = new SMSNotification("+380123456789", logger);
+const pushChannel = new PushNotification("device-token-abc", logger);
+
+
+const notificationService = new NotificationService([
+  emailChannel,
+  smsChannel,
+  pushChannel
+]);
+
+
+const user = new User(
+  "example@email.com",
+  "+380123456789",
+  "device-token-abc",
+  notificationService
+);
 
 user.sendNotification("Ваш платіж оброблено успішно!");
