c63c075b481e07f1d687c684d12682b64e9eebde
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-28 01:51:15 +0300

    Update SMSNotification.ts

--- hw02/src/services/SMSNotification.ts
+++ hw02/src/services/SMSNotification.ts
@@ -1,1 +1,13 @@
-// TODO: Implement the SMSNotification class
+import { INotification, ILogger } from "../core/interfaces";
+
+export class SMSNotification implements INotification {
+  constructor(
+    private readonly recipient: string,
+    private readonly logger: ILogger
+  ) {}
+
+  send(to: string, message: string): void {
+    this.logger.log(`Sending SMS to ${to}`);
+    console.log(`SMS sent to ${to}: ${message}`);
+  }
+}
