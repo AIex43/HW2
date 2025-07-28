4efc3c5b3e711b4faa6b83d67b657027a6670b8c
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-28 19:07:51 +0300

    Update interfaces.ts

--- hw02/src/core/interfaces.ts
+++ hw02/src/core/interfaces.ts
@@ -1,16 +1,11 @@
-import { User } from '../models/User';
-
-export interface INotificationChannel {
-    send(message: string): void;
+export interface ILogger {
+  log(message: string): void;
 }
 
-export interface ILogger {
-    log(message: string): void;
+export interface INotification {
+  send(recipient: string, message: string): void;
 }
 
 export interface INotificationService {
-    addChannel(channel: INotificationChannel): void;
-    sendEmail(user: User, message: string): void;
-    sendSMS(user: User, message: string): void;
-    sendPush(user: User, message: string): void;
+  notifyAll(message: string): void;
 }
